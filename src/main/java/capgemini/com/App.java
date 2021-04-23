63cb7e0d27879bab575924e8686d8b6c034ded2d
root <root@ip-172-31-31-31.eu-west-2.compute.internal>
2021-04-23 04:38:29 +0000

    commiting new project

---
+++ src/main/java/capgemini/com/App.java
@@ -0,0 +1,13 @@
+package capgemini.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
